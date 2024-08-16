03992a8db3bde1f3f3117adcec8ab5297ff295d1
boilertentwo <chakriganeshn@gmail.com>
2024-08-17 03:46:41 +0530

    push it now

--- app/(user)/model/[modelID]/[itemID]/page.jsx
+++ app/(user)/model/[modelID]/[itemID]/page.jsx
@@ -7,7 +7,6 @@ export default function ImageForm({params}){
             <div className="min-h-full min-w-full p-6 flex flex-col items-center">
                  <CldImage alt="crafted image" src={`${params.modelID}/${params.itemID}`} width='100' height='100'/>
             </div>
-            
         </>
     )
 }
