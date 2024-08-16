11dca52a0dc1ea834f0121cbeb438d2d91b5be01
boilertentwo <chakriganeshn@gmail.com>
2024-08-17 04:06:53 +0530

    trying to modify image proportion

--- components/TestComponent.jsx
+++ components/TestComponent.jsx
@@ -8,7 +8,7 @@ import { Skeleton } from '@/components/ui/skeleton'; // Import your skeleton com
 export default function ClientImageGallery({ images }) {
   const router = useRouter();
   return (
-    <div className="h-auto w-auto flex flex-row justify-around items-start p-4 mx-4 scroll-mx-px snap-mandatory snap-x overflow-x-auto gap-4">
+    <div className="h-auto w-full flex flex-row justify-around items-start p-4 mx-4 scroll-mx-px snap-mandatory snap-x overflow-x-auto gap-4">
       {!images ? (
         <Skeleton className="h-[75px] w-[75px]"></Skeleton> // Adjust size to match your images
       ) : (
