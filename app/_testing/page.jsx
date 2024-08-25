9ef8d3d60ac1abba3d38e6713e343e86ab83e5ad
boilertentwo <chakriganeshn@gmail.com>
2024-08-26 02:03:35 +0530

    removed testing

--- app/_testing/page.jsx
+++ app/_testing/page.jsx
@@ -1,32 +1,32 @@
-'use client'
-import { useState } from 'react';
-import ProgressBar from '@/components/testing/Test1.jsx';
+// 'use client'
+// import { useState } from 'react';
+// import ProgressBar from '@/components/testing/Test1.jsx';
 
-export default function Home() {
-  const [status, setStatus] = useState('verified');
+// export default function Home() {
+//   const [status, setStatus] = useState('verified');
 
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-xl font-bold mb-4">Order Progress</h1>
-      <ProgressBar verified={true} crafted={true} delivered={false} />
+//   return (
+//     <div className="container mx-auto p-4">
+//       <h1 className="text-xl font-bold mb-4">Order Progress</h1>
+//       <ProgressBar verified={true} crafted={true} delivered={false} />
       
-      <div className="mt-10">
-        <button
-          onClick={() => setStatus('verified')}
-          className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
-        >
-          Verified
-        </button>
-        <button
-          onClick={() => setStatus('full')}
-          className="bg-green-500 text-white px-4 py-2 rounded"
-        >
-          Full Progress
-        </button>
-      </div>
-    </div>
-  );
-}
+//       <div className="mt-10">
+//         <button
+//           onClick={() => setStatus('verified')}
+//           className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
+//         >
+//           Verified
+//         </button>
+//         <button
+//           onClick={() => setStatus('full')}
+//           className="bg-green-500 text-white px-4 py-2 rounded"
+//         >
+//           Full Progress
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
 
 
 
